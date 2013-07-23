8c2dab54ebaf9ccb7df4e6a9d9969a0d2d59c15e
mytstormy <mytstormy@hotmail.com>
2013-07-23 01:25:40 -0600

    Alpha 0.1a Test

--- wastelandfrontier/footer.php
+++ wastelandfrontier/footer.php
@@ -1,2 +1,4 @@
 <hr align="center" width="400" size="2" />
-<center>Wasteland Frontier Alpha 0.1 | Wasteland Frontier &copy; 2012, Savage Computer Technologies </center>
+<center>
+  Wasteland Frontier Alpha 0.1a | Wasteland Frontier &copy; 2012, Savage Computer Technologies 
+</center>
